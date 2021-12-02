1243fc36009d4f431edb179c807cb9accd622708
Nicholas Lydon <nickkell@gmail.com>
2021-12-02 18:11:14 +0100

    move json parser to own dir

---
+++ src/test/kotlin/parsingJsonExample/JsonParser.kt
@@ -0,0 +1,52 @@
+package parsingJsonExample
+
+import JsonToken
+import Parser
+import between
+import bind
+import char
+import decimal
+import defer
+import delimitedBy
+import except
+import item
+import many
+import map
+import optional
+import or
+import parse
+import skipRight
+import symbol
+import text
+import token
+
+class JsonParser {
+    private val nullP = symbol("null").map { JsonToken.JsonNull }.token()
+    private val boolP = symbol("true").map { true }.or(symbol("false").map { false }).map { JsonToken.JsonBool(it) }.token()
+    private val stringP =
+        char('\\').bind { item.map { c -> c } }.or(item.except(char('"'))).many()
+            .between(char('"'), char('"'))
+            .text()
+            .map { JsonToken.JsonString(it) }
+            .token()
+    private val numP = decimal.map { JsonToken.JsonNumber(it) }.token()
+    private val literalP = nullP.or(boolP).or(stringP).or(numP)
+    private fun arrayP() : Parser<JsonToken> =
+        defer { jsonTokenP() }
+            .delimitedBy(char(','))
+            .optional()
+            .between(char('['), char(']'))
+            .map { JsonToken.JsonArray(it.valueOrDefault(listOf())) }
+            .token()
+    private fun objectP() =
+        stringP.skipRight(char(':').token())
+            .bind { key -> jsonTokenP().map { value -> Pair(key.value, value) } }
+            .delimitedBy(char(','))
+            .optional()
+            .between(char('{'), char('}'))
+            .map { JsonToken.JsonObject(it.valueOrDefault(listOf())) }
+            .token()
+    private fun jsonTokenP() : Parser<JsonToken> = literalP.or(objectP()).or(arrayP())
+
+    fun parse(input: String) = jsonTokenP().parse(input)
+}
--- src/test/kotlin/parsingJsonExample/JsonParserTest.kt
+++ src/test/kotlin/parsingJsonExample/JsonParserTest.kt
@@ -1,49 +1,8 @@
+import parsingJsonExample.JsonParser
 import org.junit.Test
 import kotlin.test.assertEquals
 
-sealed class JsonToken {
-    object JsonNull : JsonToken()
-    data class JsonString(val value: String) : JsonToken()
-    data class JsonBool(val value: Boolean) : JsonToken()
-    data class JsonNumber(val value: Double) : JsonToken()
-    data class JsonArray(val value: Iterable<JsonToken>) : JsonToken()
-    data class JsonObject(val value: Iterable<Pair<String, JsonToken>>) : JsonToken()
-}
-
-class JsonParser {
-    private val nullP = symbol("null").map { JsonToken.JsonNull }.token()
-    private val boolP = symbol("true").map { true }.or(symbol("false").map { false }).map { JsonToken.JsonBool(it) }.token()
-    private val stringP =
-        char('\\').bind { item.map { c -> c } }.or(item.except(char('"'))).many()
-            .between(char('"'), char('"'))
-            .map { JsonToken.JsonString(it.joinToString("")) }
-            .token()
-    private val numP = decimal.map { JsonToken.JsonNumber(it) }.token()
-    private val literalP = nullP.or(boolP).or(stringP).or(numP)
-    private fun arrayP() : Parser<JsonToken> =
-        defer { jsonTokenP() }
-            .delimitedBy(char(','))
-            .optional()
-            .between(char('['), char(']'))
-            .map {JsonToken.JsonArray(it.valueOrDefault(listOf())) }
-            .token()
-    private fun objectP() =
-        stringP.skipRight(char(':').token())
-            .bind { key -> jsonTokenP().map { value -> Pair(key.value, value) } }
-            .delimitedBy(char(','))
-            .optional()
-            .between(char('{'), char('}'))
-            .map { JsonToken.JsonObject(it.valueOrDefault(listOf())) }
-            .token()
-    private fun jsonTokenP() : Parser<JsonToken> =
-        literalP
-            .or(objectP())
-            .or(arrayP())
-
-    fun parse(input: String) = jsonTokenP().parse(input)
-}
-
-class Json {
+class JsonParserTest {
 
     @Test
     fun tokenEquality() {
@@ -57,17 +16,17 @@ class Json {
         val json =
             """
                 {
-                    "StringName": "String Value",
+                    "StringName": "String \"Value\"",
                     "BoolName": true,
                     "ArrayName": [1, "hello", false, {}],
                     "ObjectName": {
                         "NestedProperty": []
-                    }
+                    }                    
                 }
             """
         val expected = JsonToken.JsonObject(
             listOf(
-                Pair("StringName", JsonToken.JsonString("String Value")),
+                Pair("StringName", JsonToken.JsonString("String \"Value\"")),
                 Pair("BoolName", JsonToken.JsonBool(true)),
                 Pair(
                     "ArrayName", JsonToken.JsonArray(
@@ -104,7 +63,7 @@ class Json {
 
     @Test
     fun stringP2() {
-        assertEquals("\"\\hello!", when (val x = JsonParser().parse(""" "\"\\hello!" """)) {
+        assertEquals(" \" \\ hello! ", when (val x = JsonParser().parse(""" " \" \\ hello! " """)) {
             is JsonToken.JsonString -> x.value
             else -> null
         })
